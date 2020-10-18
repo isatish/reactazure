9a1206aaf3787510e8911a937a94913673b3f187
satishtarange <satishtarange@gmail.com>
2020-10-18 19:11:49 +0530

    azure webservice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+let name = 'Satish';
+  const sayHello = (userName) => {
+    const code = 'FBPUcCxnS8xPLrQuTWthjplaTpK1x5Ob3pVDppl1b3up3LWwgYOSGQ==';
+    fetch(`https://sendsmstomobile.azurewebsites.net/api/HTUpdateCosmos?code=${code}&name=${userName}`)
+        .then(res => console.log(res))
+        .catch(console.log)
+      }
+  
+  const onChangeHandler = (e) =>{
+    name = e.target.value;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello Azure</p>
       </header>
+      <div className="App-container">
+        <input type="text" onChange={onChangeHandler}/>
+        <button onClick={()=>sayHello(name)}>Say Hello</button>
+      </div>
     </div>
   );
 }
