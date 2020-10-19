b69b4c639322037e3803535906ffeadb5c3f797c
satishtarange <satishtarange@gmail.com>
2020-10-19 10:27:28 +0530

    updated api call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
+//https://sendsmstomobile.azurewebsites.net/api/HttpTriggerUpdateCosmosDB?code=cY8VsSEJtq518zu4Waxoz30rZkPCaEbVj10wndUoaXvQ24E36dmepw==&name=pramod
 function App() {
-let name = 'Satish';
+  let name = 'Satish';
+  const [azResponse, setData] = useState();
   const sayHello = (userName) => {
-    const code = 'FBPUcCxnS8xPLrQuTWthjplaTpK1x5Ob3pVDppl1b3up3LWwgYOSGQ==';
-    fetch(`https://sendsmstomobile.azurewebsites.net/api/HTUpdateCosmos?code=${code}&name=${userName}`)
-        .then(res => console.log(res))
-        .catch(console.log)
-      }
-  
-  const onChangeHandler = (e) =>{
+    const code = 'cY8VsSEJtq518zu4Waxoz30rZkPCaEbVj10wndUoaXvQ24E36dmepw==';
+    axios.get(`https://sendsmstomobile.azurewebsites.net/api/HttpTriggerUpdateCosmosDB?code=${code}&name=${userName}`)
+      .then(res => {
+        console.log(res);
+        setData(res.data);
+      })
+  }
+
+  const onChangeHandler = (e) => {
     name = e.target.value;
   }
 
@@ -20,8 +25,10 @@ let name = 'Satish';
         <p>Azure Demo</p>
       </header>
       <div className="App-container">
-        <input type="text" onChange={onChangeHandler}/>
-        <button onClick={()=>sayHello(name)}>Say Hello</button>
+        <input type="text" onChange={onChangeHandler} />
+        <button onClick={() => sayHello(name)}>Say Hello</button>
+        <br />
+        <div>{azResponse}</div>
       </div>
     </div>
   );
